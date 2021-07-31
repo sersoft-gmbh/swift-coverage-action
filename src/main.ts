123886b882c1ce341d9a2b654be4f73d5358f311
Florian Friedrich <ffried@me.com>
2021-07-31 15:04:32 +0200

    Skip descendants of found targets

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,15 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 import { promises as fs, existsSync as exists, PathLike } from 'fs';
-import { resolve, dirname } from 'path';
-import * as path from "path";
-import * as os from "os";
+import * as path from 'path';
+import * as os from 'os';
 
 async function runCmd(cmd: string, args?: string[]): Promise<string> {
     const output = await exec.getExecOutput(cmd, args, {
         silent: !core.isDebug(),
     });
-    if (output.stderr.length > 0) {
+    if (output.stderr.length > 0)
         core.warning(`Command execution wrote lines to stderr:\n${output.stderr}`);
-    }
     return output.stdout;
 }
 
@@ -24,19 +22,23 @@ enum CovFormat {
 declare type WalkEntry = {
   path: string;
   isDirectory: boolean;
+
+  skipDescendants(): void;
 };
 
 // Taken and adjusted from https://stackoverflow.com/a/65415138/1388842
 async function* walk(dir: string, onlyFiles: boolean = true): AsyncGenerator<WalkEntry> {
     const entries = await fs.readdir(dir, { withFileTypes: true });
     for (const entry of entries) {
-        const res = resolve(dir, entry.name);
+        const res = path.resolve(dir, entry.name);
         if (entry.isDirectory()) {
+            let skipDesc = false;
             if (!onlyFiles)
-                yield { path: res, isDirectory: true };
-            yield* walk(res, onlyFiles);
+                yield { path: res, isDirectory: true, skipDescendants: () => skipDesc = true };
+            if (!skipDesc)
+                yield* walk(res, onlyFiles);
         } else {
-            yield { path: res, isDirectory: false };
+            yield { path: res, isDirectory: false, skipDescendants: () => {} };
         }
     }
 }
@@ -65,10 +67,10 @@ async function main() {
             .map(p => path.resolve(p.replace(/(~|\$HOME|\${HOME})/g, os.homedir)));
     const outputFolder = path.resolve(core.getInput('output', { required: true })
         .replace(/(~|\$HOME|\${HOME})/g, os.homedir));
-    const format = CovFormat[core.getInput('format', { required: true }) as keyof typeof CovFormat];
-    if (!format) {
-        throw new Error('Invalid format');
-    }
+    const _format = core.getInput('format', { required: true })
+    const format = CovFormat[_format as keyof typeof CovFormat];
+    if (!format)
+        throw new Error(`Invalid format: ${_format}`);
     const _targetNameFilter = core.getInput('target-name-filter');
     const targetNameFilter = _targetNameFilter ? new RegExp(_targetNameFilter) : null;
     const ignoreConversionFailures = core.getBooleanInput('ignore-conversion-failures');
@@ -94,7 +96,7 @@ async function main() {
                 }
             }
         }
-        core.info(`Found ${profDataFiles.length} profiling data file(s)...`);
+        core.info(`Found ${profDataFiles.length} profiling data file(s):\n${profDataFiles.join('\n')}`);
         return profDataFiles;
     });
 
@@ -103,25 +105,31 @@ async function main() {
         convertedFiles = await core.group('Converting files', async () => {
             let outFiles: string[] = [];
             let conversionFailures: Error[] = [];
+            let processedTargets = new Set<string>();
             for (const profDataFile of profDataFiles) {
-                const profDataDir = dirname(profDataFile);
+                const profDataDir = path.dirname(profDataFile);
                 const xcodeRegex = /(Build).*/;
                 let buildDir: string;
                 if (xcodeRegex.test(profDataDir)) {
                     buildDir = profDataDir.replace(xcodeRegex, '$1');
-                } else {
-                    buildDir = dirname(profDataDir);
+                } else { // SPM
+                    buildDir = path.dirname(profDataDir);
                 }
                 core.debug(`Checking contents of build dir ${buildDir} of prof data file ${profDataFile}`);
                 for await (const entry of walk(buildDir, false)) {
                     const typesRegex = /.*\.(app|framework|xctest)$/;
                     if (!typesRegex.test(entry.path)) continue;
+                    entry.skipDescendants(); // Don't process any further files inside this container.
                     const type = entry.path.replace(typesRegex, '$1');
                     core.debug(`Found match of type ${type}: ${entry.path}`);
                     const proj = entry.path
                         .replace(/.*\//, '')
                         .replace(`.${type}`, '');
                     core.debug(`Project name: ${proj}`);
+                    if (processedTargets.has(proj)) {
+                        core.info(`Skipping ${proj} because it has already been converted...`);
+                        continue;
+                    }
                     if (targetNameFilter && !targetNameFilter.test(proj)) {
                         core.info(`Skipping ${proj} due to target name filter...`);
                         continue;
@@ -181,6 +189,7 @@ async function main() {
                     core.debug(`Writing coverage report to ${outFile}`);
                     await fs.writeFile(outFile, converted);
                     outFiles.push(outFile);
+                    processedTargets.add(proj);
                 }
             }
             if (conversionFailures.length > 0) {
@@ -190,14 +199,13 @@ async function main() {
                     throw new Error('Conversion failures:\n' + conversionFailures.map(e => e.toString()).join('\n'));
                 }
             }
-            core.info(`Processed ${outFiles.length} file(s)...`);
+            core.info(`Processed ${outFiles.length} file(s):\n${outFiles.join('\n')}`);
             return outFiles;
         });
     }
     core.setOutput('files', JSON.stringify(convertedFiles));
-    if (convertedFiles.length <= 0 && failOnEmptyOutput) {
+    if (convertedFiles.length <= 0 && failOnEmptyOutput)
         throw new Error('No coverage files found (or none succeeded to convert)!');
-    }
 }
 
 try {
