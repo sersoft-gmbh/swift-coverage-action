04ad50794775e6f14302c2c9e3ea9fff0c33eee0
Florian Friedrich <ffried@me.com>
2021-07-31 15:12:06 +0200

    Include type in duplicate check

--- src/main.ts
+++ src/main.ts
@@ -126,8 +126,8 @@ async function main() {
                         .replace(/.*\//, '')
                         .replace(`.${type}`, '');
                     core.debug(`Project name: ${proj}`);
-                    if (processedTargets.has(proj)) {
-                        core.info(`Skipping ${proj} because it has already been converted...`);
+                    if (processedTargets.has(`${proj}.${type}`)) {
+                        core.info(`Skipping ${proj} with type ${type} because it has already been converted...`);
                         continue;
                     }
                     if (targetNameFilter && !targetNameFilter.test(proj)) {
@@ -189,7 +189,7 @@ async function main() {
                     core.debug(`Writing coverage report to ${outFile}`);
                     await fs.writeFile(outFile, converted);
                     outFiles.push(outFile);
-                    processedTargets.add(proj);
+                    processedTargets.add(`${proj}.${type}`);
                 }
             }
             if (conversionFailures.length > 0) {
